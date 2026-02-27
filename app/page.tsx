f5d79d5eb4a20953cbed1304609345d2dbd2af20
connerchu <connerchu@berkeley.edu>
2026-02-27 14:35:30 -0800

    update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
         Conner Chu
       </h1>
       <p className="mb-4">
-        Hi! I am a third-year undergraduate at UC Berkeley studying physics and applied mathematics. 
-        Surprisingly, I actually entered university as a biology major interested in neuroscience.
+        Hi! I am a third-year undergraduate at UC Berkeley studying physics and applied math. 
+        I actually entered university as a biology major interested in neuroscience.
         During that time, I found myself involved with{' '}
         <a
           className="underline"
@@ -19,18 +19,20 @@ export default function Page() {
           Neurotech@Berkeley
         </a>{' '}
         where I explored wetware computing. Seeking a more general understanding of reality, I
-        switched to physics and applied mathematics in the second half of my first year. The
-        following summer, I studied the dynamics of semiconductors using attosecond transient
-        absorption spectroscopy under Professor Stephen Leone in the{' '}
+        switched to physics and applied math, where I've been fortunate enough to partake in research on and off campus.
+        <br /> 
+        <br />
+        In the{' '}
         <a
           className="underline"
           href="https://sites.google.com/berkeley.edu/leonegrp"
           target="_blank"
           rel="noreferrer"
         >
-          Leone Group
-        </a>
-        . I then spent time with the{' '}
+          Leone Group,
+        </a>{' '}
+        I've spent time studying the dynamics of semiconductors using attosecond transient absorption spectroscopy. 
+        I've also worked on detector calibration with the{' '}
         <a
           className="underline"
           href="https://www.physics.lbl.gov/lfp/mu2e/"
@@ -39,8 +41,7 @@ export default function Page() {
         >
           Mu2e Group
         </a>{' '}
-        at Lawrence Berkeley National Laboratory under Dr. Richard Bonventre working on detector
-        calibration. Most recently, I was at the{' '}
+        at Lawrence Berkeley National Laboratory. Most recently, I was at the{' '}
         <a
           className="underline"
           href="https://www.aei.mpg.de/continuouswaves"
@@ -49,13 +50,18 @@ export default function Page() {
         >
           Max Planck Institute for Gravitational Physics
         </a>{' '}
-        in Germany working with Professor Maria Alessandra Papa and Dr. Benjamin Steltner searching
-        for continuous gravitational waves. Recently, I've taken interest in applying the formalism 
-        of physics to fundamentally understand deep learning.
+        in Germany searching for continuous gravitational waves. 
+        I've also taken interest in deep learning, where I work with a small group at the{' '}
+        <a
+          className="underline"
+          href="https://redwood.berkeley.edu/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Redwood Center for Theoretical Neuroscience
+        </a>{' '}
+        trying to understand neural networks via kernel methods.
       </p>
-      <div className="mt-4 mb-10">
-        <BlogPosts />
-      </div>
     </section>
   )
 }
